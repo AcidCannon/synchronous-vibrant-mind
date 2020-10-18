d5c445996c5923ca390d0a066e7585262fb9c2d2
boyuandong <bdong@ualberta.ca>
2020-10-18 12:32:38 -0600

    modified layout

--- my-svm/src/components/home_select_date.js
+++ my-svm/src/components/home_select_date.js
@@ -1,25 +1,48 @@
 import React, { Fragment, useState } from "react";
 import DateFnsUtils from '@date-io/date-fns';
-import { DatePicker, TimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import {  KeyboardDatePicker, KeyboardTimePicker, MuiPickersUtilsProvider } from "@material-ui/pickers";
+import { Grid } from "@material-ui/core";
 
 function InlineTimePickerDemo() {
     const [selectedDate, handleDateChange] = useState(new Date());
 
     return (
         <MuiPickersUtilsProvider utils={DateFnsUtils}>
-            <DatePicker
+            <Grid 
+            container 
+            justify="flex-start"
+            alignItems="center" 
+            direction="row" 
+            >
+                
+            <KeyboardDatePicker 
+                disableToolbar
                 variant="inline"
+                format="MM/dd/yyyy"
+                margin="normal"
+                id="date-picker-inline"
                 label="Date"
                 value={selectedDate}
                 onChange={handleDateChange}
+                KeyboardButtonProps={{
+                    'aria-label': 'change date',
+                  }}
             />
-
-            <TimePicker
-                variant="inline"
+             <div style={{width: 100}} />
+            <KeyboardTimePicker
+                margin="normal"
+                id="time-picker"
                 label="Time"
                 value={selectedDate}
                 onChange={handleDateChange}
+                KeyboardButtonProps={{
+                    'aria-label': 'change time',
+                  }}
             />
+
+            </Grid>
+           
+            
         </MuiPickersUtilsProvider>
     );
 }
