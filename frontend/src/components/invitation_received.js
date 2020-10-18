8ad4d437bb29bcfceadae26ed2c85c6112e994e2
AcidCannon <zxi3@ualberta.ca>
2020-10-19 05:49:00 +0800

    Populated frontend from react-login branch.

---
+++ frontend/src/components/invitation_received.js
@@ -0,0 +1,22 @@
+import { Grid } from '@material-ui/core';
+import React, { Component } from 'react';
+import Table from './table_invitation_received';
+
+class InvitationReceived extends Component {
+    constructor(props){
+        super(props);
+        this.state={}
+    }
+
+    render() {
+      return (
+        <div>
+          <Grid container justify="center" style={{color: "#3291ff" }}>
+            <h1>Invitation Received</h1>
+          </Grid>
+          <Table />
+        </div>
+      );
+    }
+  }
+  export default InvitationReceived;
