40b753872eb60f8ff338c788ff0ac71843485803
AcidCannon <zxi3@ualberta.ca>
2020-11-03 03:21:23 +0800

    Conflict Resolved.

--- backend/entrypoint.sh
+++ backend/entrypoint.sh
@@ -1,7 +1,13 @@
 #!/bin/sh
 
-python manage.py flush --no-input
-python manage.py migrate
-python manage.py collectstatic --no-input --clear
+if [ ! -f "FirstRunFlag" ]; then
+    echo DO NOT DELETE THIS FILE UNLESS YOU KNOW WHAT YOU ARE DOING! > FirstRunFlag
+    python manage.py makemigrations
+    python manage.py migrate
+    python manage.py flush --no-input
+    python manage.py collectstatic --no-input --clear
+else
+    python manage.py collectstatic --no-input --clear
+fi
 
 exec "$@"
