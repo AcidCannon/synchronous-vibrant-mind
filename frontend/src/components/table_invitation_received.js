3b179af051a868afb9cffcbc9705fcabb32cbd71
boyuandong <bdong@ualberta.ca>
2020-11-04 14:30:18 -0700

    update received buttons

--- frontend/src/components/table_invitation_received.js
+++ frontend/src/components/table_invitation_received.js
@@ -12,46 +12,89 @@ import Button from '@material-ui/core/Button';
 import moment from 'moment';
 
 const columns = [
-  { id: 'inviter', label: 'Inviter', minWidth: 170 },
+  { id: 'inviter', label: 'Inviter', minWidth: 170},
   { id: 'gamedate', label: 'Game date', minWidth: 100 },
   {
     id: 'game_start_time',
     label: 'Game start time',
     minWidth: 170,
     align: 'right',
-    format: (value) => value.toLocaleString('en-US'),
+    format: (value) => value.toLocaleString('en-US')
   },
 
   {
     id: 'accept',
     label: 'Accept',
     minWidth: 170,
-    align: 'right',
-    // format: (value) => value.toLocaleString('en-US'),
+    align: 'right'
   },
   {
     id: 'decline',
     label: 'Decline',
     minWidth: 170,
-    align: 'right',
-    // format: (value) => value.toFixed(2),
+    align: 'right'
   },
 ];
 
-function createData(inviter, gamedate, game_start_time, accept, decline) {
-  // const density = game_start_time / size;
-  return { inviter, gamedate, game_start_time, accept, decline };
+function createData(inviter, invitee, id, gamedate, game_start_time) {
+  return { inviter, invitee, id, gamedate, game_start_time };
 }
 
-const rows = [
-  createData('Alpha', '2020-01-23', 8,<Button variant="contained" color="primary">Accept</Button>, <Button variant="contained" color="primary">Decline</Button>),
-  createData('Bdong', '2020-05-26', 8, <Button variant="contained" color="primary">Accept</Button>, <Button variant="contained" color="primary">Decline</Button>),
-  createData('Zoe', '2020-06-01', 8, <Button variant="contained" color="primary">Accept</Button>, <Button variant="contained" color="primary">Decline</Button>),
-  createData('Zuhao', '2020-06-01', 8, <Button variant="contained" color="primary">Accept</Button>, <Button variant="contained" color="primary">Decline</Button>),
-  createData('Zijian', '2020-06-01', 8, <Button variant="contained" color="primary">Accept</Button>, <Button variant="contained" color="primary">Decline</Button>),
-  createData('Zihao', '2020-06-01', 8, <Button variant="contained" color="primary">Accept</Button>, <Button variant="contained" color="primary">Decline</Button>),
+const rows = [];
 
-];
+async function sentNotification(inviter, invitee, clicked_status, id){
+  if (clicked_status == "ACCEPTED"){
+    var player_content = invitee + " accepted your invitation";
+  }else if (clicked_status == "DECLINED"){
+    var player_content = invitee + " is bussy";
+  }
+  const response = await fetch("http://localhost/api/sendNotification", {
+    method: "POST",
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ username: inviter, invitation_id: id, content: player_content})
+    }).then(async response => {
+      const data = await response.json();
+      console.log('sendNotification body',JSON.stringify({ username: inviter, invitation_id: id, content: player_content}));
+      // check for error response
+      if (!response.ok) {
+          // get error message from body or default to response status
+          const error = (data && data.message) || response.status;
+          return Promise.reject(error);
+      }
+  }).catch(error => {
+      console.error('There was an error!', error);
+  });
+ 
+}
+
+async function changeInvitationStatus(inviter, invitee, clicked_status, id){
+  const response = await fetch("http://localhost/api/changeInvitationStatus", {
+    method: "POST",
+      headers: { 
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify({ invitation_id: id, status: clicked_status})
+    }).then(async response => {
+      const data = await response.json();
+      console.log('this is the changeInvitationState json', JSON.stringify({ invitation_id: id, status: clicked_status}));
+      // check for error response
+      if (!response.ok) {
+          // get error message from body or default to response status
+          const error = (data && data.message) || response.status;
+          return Promise.reject(error);
+      }
+
+      // await new Promise((resolve, reject) => setTimeout(resolve, 1000));
+      sentNotification(inviter, invitee, clicked_status, id);
+      console.log('successful', response.status);
+
+  }).catch(error => {
+      console.error('There was an error!', error);
+  });
+ 
+}
 
 const useStyles = makeStyles({
   root: {
@@ -68,7 +111,7 @@ export default function StickyHeadTable() {
   const classes = useStyles();
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
-  const [rows, updateRows, setRows] = React.useState([]);
+  const [rows, updateRows] = React.useState([]);
 
   const handleChangePage = (event, newPage) => {
     setPage(newPage);
@@ -79,68 +122,12 @@ export default function StickyHeadTable() {
     setPage(0);
   };
 
-
+  const handleUpdateRows = (event, newRows) => {
+    updateRows(newRows);
+  };
 
 
   React.useEffect(function effectFunction() {
-
-    async function sentNotification(inviter, invitee, clicked_status, id){
-      if (clicked_status == "ACCEPTED"){
-        var player_content = invitee + " accepted your invitation";
-      }else if (clicked_status == "DECLINED"){
-        var player_content = invitee + " is bussy";
-      }
-      const response = await fetch("http://localhost/api/sendNotification", {
-        method: "POST",
-          headers: { 
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ username: inviter, invitation_id: id, content: player_content})
-        }).then(async response => {
-          const data = await response.json();
-          console.log('sendNotification body',JSON.stringify({ username: inviter, invitation_id: id, content: player_content}));
-          // check for error response
-          if (!response.ok) {
-              // get error message from body or default to response status
-              const error = (data && data.message) || response.status;
-              return Promise.reject(error);
-          }
-    
-      }).catch(error => {
-          console.error('There was an error!', error);
-      });
-     
-    }
-
-    async function changeInvitationStatus(inviter, invitee, clicked_status, id){
-      const response = await fetch("http://localhost/api/changeInvitationStatus", {
-        method: "POST",
-          headers: { 
-            'Content-Type': 'application/json'
-          },
-          body: JSON.stringify({ invitation_id: id, status: clicked_status})
-        }).then(async response => {
-          const data = await response.json();
-          console.log('this is the changeInvitationState json', JSON.stringify({ invitation_id: id, status: clicked_status}));
-          // check for error response
-          if (!response.ok) {
-              // get error message from body or default to response status
-              const error = (data && data.message) || response.status;
-              return Promise.reject(error);
-          }
-
-          await new Promise((resolve, reject) => setTimeout(resolve, 1000));
-          sentNotification(inviter, invitee, clicked_status, id);
-          console.log('successful');
-          
-  
-    
-      }).catch(error => {
-          console.error('There was an error!', error);
-      });
-     
-    }
-
     async function fetchInvitationReceived() {
       const response = await fetch("http://localhost/api/getInvitationReceived", {
         method: "POST",
@@ -160,7 +147,7 @@ export default function StickyHeadTable() {
         if( (response.status == 200) && (result.invitations.length >0)){
           //for loop method
           console.log("this is the response of bdong", result.invitations);
-          for (var row of result.invitations){
+          for (var row of result.invitations){ 
             if (row.state == "PENDING"){
               var gamedate = moment.utc(row.start_time).format('YYYY-MM-DD').toString();
               var game_start_time = moment.utc(row.start_time).format('hh:mm a').toString();
@@ -168,15 +155,18 @@ export default function StickyHeadTable() {
               newRows.push(
                 createData(
                   row.inviter, 
+                  row.invitee,
+                  row.id,
                   gamedate, 
-                  game_start_time, 
-                  <Button variant="contained" color="primary" onClick={()=> {changeInvitationStatus(row.inviter, row.invitee, "ACCEPTED", row.id)}}>Accept</Button>, 
-                  <Button variant="contained" color="primary" onClick={()=> {changeInvitationStatus(row.inviter, row.invitee, "DECLINED", row.id)}}>Decline</Button>)
+                  game_start_time
+                  )
                   );
 
+                console.log("this is the newRows", newRows);
+
             }
-            
           }
+          console.log("this is the newRows", newRows);
         }
         updateRows(newRows);
       }
@@ -202,15 +192,46 @@ export default function StickyHeadTable() {
           </TableHead>
           <TableBody>
             {rows.slice(page * rowsPerPage, page * rowsPerPage + rowsPerPage).map((row) => {
+              console.log("row id:", row);
               return (
                 <TableRow hover role="checkbox" tabIndex={-1} key={row.code}>
                   {columns.map((column) => {
                     const value = row[column.id];
-                    return (
+                    switch (column.id) {
+                      case "accept":   
+                      return (
                       <TableCell key={column.id} align={column.align}>
-                        {column.format && typeof value === 'number' ? column.format(value) : value}
-                      </TableCell>
-                    );
+                        <Button 
+                        variant="contained" 
+                        color="primary" 
+                        onClick={()=> {changeInvitationStatus(row.inviter, row.invitee, "ACCEPTED", row.id)}} 
+                        rows = {rows}
+                        onChangeRows = {handleUpdateRows}
+                        >
+                          Accept
+                        </Button>
+                        </TableCell>);break;
+                      case "decline": 
+                      return (
+                      <TableCell key={column.id}  align={column.align}>
+                        <Button 
+                        variant="contained" 
+                        color="primary" 
+                        onClick={()=> {changeInvitationStatus(row.inviter, row.invitee, "DECLINED", row.id)}}
+                        rows = {rows}
+                        onChangeRows = {handleUpdateRows}
+                        >
+                          Decline
+                        </Button>
+                        </TableCell>);break;
+                      default:      
+                      return (
+                        <TableCell key={column.id} align={column.align}>
+                          {column.format && typeof value === 'number' ? column.format(value) : value}
+                        </TableCell> 
+                      );break;
+                    }
+                    
                   })}
                 </TableRow>
               );
