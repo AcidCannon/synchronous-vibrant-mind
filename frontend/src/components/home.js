21c8fe93eec836b3baef7ffccb30bb30a7e0f9db
yziyi <yziyi@ualberta.ca>
2020-11-14 15:11:51 -0700

    sedn

--- frontend/src/components/home.js
+++ frontend/src/components/home.js
@@ -28,37 +28,184 @@ const styles = {
 //     },
 // }));
 
-async function sendInvitation(my_email, player_email, game_start_time){
-    const response = await fetch("http://localhost/api/addInvitation", {
-        method: "POST",
+async function sentNotification(inviter, invitee, clicked_status, id){
+    if (clicked_status == "PENDING"){
+      var player_content = "You have successfully sent your invitation to " + invitee;
+    }else if (clicked_status == "DECLINED"){
+      var player_content = invitee + " is bussy";
+    }
+    const response = await fetch("http://localhost/api/sendNotification", {
+      method: "POST",
         headers: { 
-            'Content-Type': 'application/json'
+          'Content-Type': 'application/json'
         },
-        body: JSON.stringify({ inviter_email: my_email, invitee_email: player_email, status: "PENDING", start_time: game_start_time })
-        }).then(async response => {
-        const result = await response.json();
-
+        body: JSON.stringify({ username: inviter, invitation_id: id, content: player_content})
+      }).then(async response => {
+        const data = await response.json();
+        console.log('sendNotification body',JSON.stringify({ username: inviter, invitation_id: id, content: player_content}));
         // check for error response
         if (!response.ok) {
             // get error message from body or default to response status
-            const error = (result && result.message) || response.status;
-                return Promise.reject(error);
-            }
-    
-            
-        console.log("login successful");
-
-        }).catch(error => {
-                console.error('There was an error!', error);
-        });
+            const error = (data && data.message) || response.status;
+            return Promise.reject(error);
+        }
+    }).catch(error => {
+        console.error('There was an error!', error);
+    });
+   
+  }
+  
+  async function checkTimeConflict(player_name, player_email, game_start_time){
+          const response = await fetch("http://localhost/api/isTimeConflict", {
+              method: "POST",
+              headers: { 
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ name: player_name, email: player_email, start_time: game_start_time})
+          }).then(async response => {
+              const data = await response.json();
+  
+              // check for error response
+              if (!response.ok) {
+                  // get error message from body or default to response status
+                  const error = (data && data.message) || response.status;
+                  return Promise.reject(error);
+              }
+  
+              return data.conflict
+              
+              
+              console.log("successful", data);
+          })
+          .catch(error => {
+              console.error('There was an error!', error);
+          });
+          
+      }
+  
+  async function addInvitation(my_email, my_name, player_name, player_email, invitation_status, game_start_time){
+          const response = await fetch("http://localhost/api/addInvitation", {
+              method: "POST",
+              headers: { 
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ inviter_email: my_email, invitee_email: player_email, status: invitation_status, start_time: game_start_time })
+          }).then(async response => {
+              const data = await response.json();
+  
+              // check for error response
+              if (!response.ok) {
+                  // get error message from body or default to response status
+                  const error = (data && data.message) || response.status;
+                  return Promise.reject(error);
+              }
+  
+              sentNotification(my_name, player_name, invitation_status, data.id);
+              
+              console.log("successful", data);
+          })
+          .catch(error => {
+              console.error('There was an error!', error);
+          });
+          
+      }
+  
+  async function checkPlayerExist(player_email){
+          const response = await fetch("http://localhost/api/checkPlayerExist", {
+              method: "POST",
+              headers: { 
+                  'Content-Type': 'application/json'
+              },
+              body: JSON.stringify({ email : player_email })
+          }).then(async response => {
+              const data = await response.json();
+  
+              // check for error response
+              if (!response.ok) {
+                  // get error message from body or default to response status
+                  const error = (data && data.message) || response.status;
+                  return Promise.reject(error);
+              }
+  
+              var player_exist = data.exist;
+              var player_name = null;
+              if(player_exist){
+                  player_name = data.info.name;
+              }
+  
+              return JSON.stringify({ exist : player_exist, name : player_name });
+              
+              console.log("successful", data);
+          })
+          .catch(error => {
+              console.error('There was an error!', error);
+          });
+          
+      }
+  
+  
+  async function sendInvitation(my_name, my_email, player_email, game_start_time){
+      
+  
+      var result = checkPlayerExist(player_email);
+      if(result.exist){
+          if(!checkTimeConflict(my_name, my_email, game_start_time) && !checkTimeConflict(result.name, player_email, game_start_time)){
+              addInvitation(my_email, my_name, result.name, player_email, "PENDING", game_start_time);
+          }
+          else{
+              //弹窗提醒
+              addInvitation(my_email, my_name, result.name, player_email, "FAILED", game_start_time);
+          }
+      }
+      else{
+          //给inviter发邮件
+          //给invitee发邮件
+          addInvitation(my_email, my_name, result.name, player_email, "FAILED", game_start_time);
+      }
+  
+  
+      // const response = await fetch("http://localhost/api/addInvitation", {
+      //     method: "POST",
+      //     headers: { 
+      //         'Content-Type': 'application/json'
+      //     },
+      //     body: JSON.stringify({ inviter_email: my_email, invitee_email: player_email, status: "PENDING", start_time: game_start_time })
+      //     }).then(async response => {
+      //     const result = await response.json();
+  
+      //     // check for error response
+      //     if (!response.ok) {
+      //         // get error message from body or default to response status
+      //         const error = (result && result.message) || response.status;
+      //             return Promise.reject(error);
+      //         }
+      
+              
+      //     console.log("login successful");
+  
+      //     }).catch(error => {
+      //             console.error('There was an error!', error);
+      //     });
 }
+  
+
 
 class Home extends Component {
     // const classes = useStyles();
-    state = {
-        selectedDate:new Date(),
-        single:''
+    constructor() {
+        super();
+        this.state = {
+            selectedDate:new Date(),
+            single:''
+        }
+        this.username = document.cookie.match('(^|;) ?' + "User name" + '=([^;]*)(;|$)');
+        this.x_username = unescape(this.username[2]);
+        this.my_name = this.x_username.slice(9,-2);
+        this.email = document.cookie.match('(^|;) ?' + "email" + '=([^;]*)(;|$)');
+        this.x_email = unescape(this.email[2]);
+        this.my_email = this.x_email.slice(10,-2);
     }
+    
 
     dateCallback = (date) =>{
         this.setState({selectedDate:date})
@@ -106,12 +253,10 @@ class Home extends Component {
                                 <InlineTimePickerDemo dateCallback={this.dateCallback} selectedDate={this.state.selectedDate}/>
                                  
                             </div>
-                            
-                            
                         </Grid>
                     
                     <Grid container justify="flex-end">
-                        <Button variant="contained" color="primary" onClick={()=> {console.log(this.state.single)}}>Sent</Button>
+                        <Button variant="contained" color="primary" onClick={()=> {sendInvitation(this.my_name, this.my_email, this.state.single, this.state.selectedDate)}}>Send</Button>
                     </Grid>
                     
                     </Paper>
