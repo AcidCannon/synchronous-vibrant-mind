c9fc740d530ad60022db0e9d924a833afc72d5aa
yziyi <56139768+yziyi@users.noreply.github.com>
2020-11-26 16:52:55 -0700

    Update webrtc.js

--- frontend/src/components/webrtc.js
+++ frontend/src/components/webrtc.js
@@ -65,8 +65,8 @@ export default class Webrtc extends Component {
             var id = this.getQueryVariable("roomName").replace( /[^\d.]/g, '' );
             id = parseInt(id);
             const y_username = this.getQueryVariable("srcId").replace(/\d+/g, '');
-            addMeetingLogoutTime(id, y_username, leave_time);
-            setTimeout(80000);
+            await addMeetingLogoutTime(id, y_username, leave_time);
+         
             
         });
 
