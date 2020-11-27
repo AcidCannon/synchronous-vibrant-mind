ccb891e5f835f7dae1ca8a70b365a31cbfe74ae7
Felix-Huang11 <54449911+Felix-Huang11@users.noreply.github.com>
2020-11-26 19:47:27 -0700

    Update tests.py

--- backend/api/tests.py
+++ backend/api/tests.py
@@ -20,6 +20,8 @@ class MyTest(unittest.TestCase):
         r2 = request.post(url, {"name": "test2", "email" : "test2@ualberta.ca"})
         self.assertEqual(r1.status_code, 200)
         self.assertEqual(r2.status_code, 200)
+        self.assertEqual(r1.json()['status'],"success")
+        self.assertEqual(r2.json()['status'],"success")
 
     #test addInvitation
     def testAddInvitation(self):
@@ -29,6 +31,8 @@ class MyTest(unittest.TestCase):
         r2 = request.post(url, {"inviter_email" : "test1@ualberta.ca", "invitee_email" : "test2@ualberta.ca", "start_time": "2020-11-1 12:00:00", "status":"FAILED"})
         self.assertEqual(r1.status_code, 200)
         self.assertEqual(r2.status_code, 200)
+        self.assertEqual(r1.json()['status'],"success")
+        self.assertEqual(r2.json()['status'],"success")
 
     #test changeInvitationStatus
     def testChangeStatus(self):
@@ -36,6 +40,7 @@ class MyTest(unittest.TestCase):
         url = '/changeInvitationStatus'
         r1 = request.post(url, {"invitation_id":1, "status":"ACCEPTED"})
         self.assertEqual(r1.status_code, 200)
+        self.assertEqual(r1.json()['status'],"success")
         
 
     #test addMeeting
@@ -44,6 +49,7 @@ class MyTest(unittest.TestCase):
         url = '/addMeeting'
         r1 = request.post(url, {"invitation_id":1})
         self.assertEqual(r1.status_code, 200)
+        self.assertEqual(r1.json()['status'],"success")
 
     #test addMeetingTime
     def testAddMeetingTime(self):
@@ -51,6 +57,7 @@ class MyTest(unittest.TestCase):
         url = '/addMeetingTime'
         r1 = request.post(url, {"invitation_id":1, "name":"test1", "login_time":"2020-11-1 12:00:00","logout_time":"2020-11-1 12:30:00"})
         self.assertEqual(r1.status_code, 200)
+        self.assertEqual(r1.json()['status'],"success")
 
     #test sendNotification
     def testSendNotification(self):
@@ -58,6 +65,7 @@ class MyTest(unittest.TestCase):
         url = '/sendNotification'
         r1 = request.post(url, {"invitation_id":1, "username":"test1", "content":"test"})
         self.assertEqual(r1.status_code, 200)
+        self.assertEqual(r1.json()['status'],"success")
 
     #test changeNotificationStatus
     def testChangeNotificationStatus(self):
@@ -65,47 +73,61 @@ class MyTest(unittest.TestCase):
         url = '/changeNotificationStatus'
         r1 = request.post(url, {"notification_id":1, "status":"READ"})
         self.assertEqual(r1.status_code, 200)
+        self.assertEqual(r1.json()['status'],"success")
 
     #test getInvitationSent
     def testGetInvitationSent(self):
         request = self.client
-        url = '/getInvitationSent'
-        r1 = request.post(url, {"email":"test1@ualberta.ca"})
+        url = '/getInvitationSent?p=test1@ualberta.ca'
+        r1 = request.get(url)
         self.assertEqual(r1.status_code, 200)
+        self.assertEqual(r1.json()['status'],"success")
+        self.assertEqual(r1.json()['invitations'][0]['inviter'], "test1")
+        self.assertEqual(r1.json()['invitations'][0]['state'], "PENDING")
+
 
     #test getInvitationReceived
     def testGetInvitationReceived(self):
         request = self.client
-        url = '/getInvitationReceived'
-        r1 = request.post(url, {"email":"test2@ualberta.ca"})
+        url = '/getInvitationReceived?p=test2@ualberta.ca'
+        r1 = request.get(url)
         self.assertEqual(r1.status_code, 200)
+        self.assertEqual(r1.json()['status'],"success")
+        self.assertEqual(r1.json()['invitations'][0]['invitee'], "test2")
+        self.assertEqual(r1.json()['invitations'][0]['state'], "PENDING")
 
     #test getNotification
     def testGetNotification(self):
         request = self.client
-        url = '/getNotification'
-        r1 = request.post(url, {"email":"test1@ualberta.ca"})
+        url = '/getNotification?p=test1@ualberta.ca'
+        r1 = request.get(url)
         self.assertEqual(r1.status_code, 200)
+        self.assertEqual(r1.json()['status'],"success")
+        self.assertEqual(r1.json()['notifications'][0]['player'], "test1")
 
     #test getMeetingHistory
     def testGetMeetingHistory(self):
         request = self.client
-        url = '/getMeetingHistory'
-        r1 = request.post(url, {"email":"test1@ualberta.ca"})
+        url = '/getMeetingHistory?p=test1@ualberta.ca'
+        r1 = request.get(url)
         self.assertEqual(r1.status_code, 200)
+        self.assertEqual(r1.json()['status'],"success")
+        self.assertEqual(len(r1.json()['history']), 0)
 
     #test getUpcomingEvent
     def testGetUpcomingEvent(self):
         request = self.client
-        url = '/getUpcomingEvent'
-        r1 = request.post(url, {"email":"test1@ualberta.ca"})
+        url = '/getUpcomingEvent?p=test1@ualberta.ca'
+        r1 = request.get(url)
         self.assertEqual(r1.status_code, 200)
+        self.assertEqual(r1.json()['status'],"success")
+        self.assertEqual(len(r1.json()['upcoming']), 0)
 
     #test isTimeConflict
     def testIsTimeConflict(self):
         request = self.client
         url = '/isTimeConflict'
-        r1 = request.post(url, {"name":"test1","email":"test1@ualberta.ca","start_time":"202011-1 12:00:00"})
+        r1 = request.post(url, {"name":"test1","email":"test1@ualberta.ca","start_time":"202011-1 21:00:00"})
         self.assertEqual(r1.status_code, 200)
 
     #test checkPlayerExist
@@ -114,6 +136,7 @@ class MyTest(unittest.TestCase):
         url = '/checkPlayerExist'
         r1 = request.post(url, {"email":"test1@ualberta.ca"})
         self.assertEqual(r1.status_code, 200)
+        self.assertEqual(r1.json()['exist'], True)
 
     #test getAllPlayer
     def testGetAllPlayer(self):
