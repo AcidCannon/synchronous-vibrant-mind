b882ca17d219de4ae3cdd00a9d9667144812f2a9
Felix-Huang11 <54449911+Felix-Huang11@users.noreply.github.com>
2020-11-26 17:08:01 -0700

    Update start-peerjs.sh

--- start-peerjs.sh
+++ start-peerjs.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-peerjs --port 9000 --key peerjs --path / --sslkey ~/synchronous-vibrant-mind/nginx/server.key --sslcert ~/synchronous-vibrant-mind/nginx/server.crt
+nohup peerjs --port 9000 --key peerjs --path / --sslkey ~/synchronous-vibrant-mind/nginx/server.key --sslcert ~/synchronous-vibrant-mind/nginx/server.crt > /dev/null 2>&1 & 
 
